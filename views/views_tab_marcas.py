3ae5262b11f47df0db331b8e2bd4ecf1803bbd51
swimprojecttraining <swimprojecttraining@gmail.com>
2026-07-10 10:13:43 -0400

    Update views_tab_marcas.py

--- views/views_tab_marcas.py
+++ views/views_tab_marcas.py
@@ -3,8 +3,14 @@ import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
+from formulas_lib_funciones import (
+    convertir_string_a_segundos,
+    calcular_edad_decimal,
+    formatear_a_minutos,
+    calcular_categoria_competencia
+)
 
-def renderizar_tab_marcas(supabase, titulo_grafico, es_preinfantil, convertir_string_a_segundos, calcular_edad_decimal, formatear_a_minutos, calcular_categoria_competencia):
+def renderizar_tab_marcas(datos_sidebar):
     st.markdown("### ⏱️ Panel de Control Curricular y Marcas Oficiales")
     st.caption("Módulo centralizado para la gestión de marcas oficiales, análisis de récords personales y exportación curricular.")
 
