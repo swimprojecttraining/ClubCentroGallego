841152f2cd52a544acef34fbd72be24516fa0add
swimprojecttraining <swimprojecttraining@gmail.com>
2026-07-10 19:14:05 -0400

    Fix import statement for supabase in views_sidebar.py

--- views/views_sidebar.py
+++ views/views_sidebar.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from conexion views_login_app.py supabase
+from views_login_app.py import supabase
 # 1. Definimos la función en el mismo archivo para prueba rápida
 def renderizar_sidebar_acceso_y_gestion(supabase):
     st.sidebar.markdown(f"**Usuario:** Alvaro Gallegos") # Ajusta según tu lógica real
