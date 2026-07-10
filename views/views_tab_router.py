8bf42f7dafa31365e25431dcc164eebe8e41667d
swimprojecttraining <swimprojecttraining@gmail.com>
2026-07-09 21:51:13 -0400

    Update views_tab_router.py

--- views/views_tab_router.py
+++ views/views_tab_router.py
@@ -9,7 +9,7 @@ from views_sidebar import renderizar_sidebar_completo
 # 2. IMPORTACIÓN DE CADA PESTAÑA DE MANERA DIRECTA
 from views_tab_admin import renderizar_tab_admin
 #from views_tab_asignaciones import renderizar_tab_asignaciones
-#from views_tab_calendario import renderizar_tab_calendario
+from views_tab_calendario import renderizar_tab_calendario
 #from views_tab_entrenador import renderizar_tab_entrenador
 #from views_tab_grafico import renderizar_tab_grafico
 #from views_tab_marcas import renderizar_tab_marcas
@@ -71,8 +71,8 @@ def mostrar_vista_enrutador():
 #            renderizar_tab_entrenador(datos_sidebar=None)
 #        with tab_asignaciones:
 #            renderizar_tab_asignaciones(datos_sidebar=None)
-#        with tab_calendario:
-#            renderizar_tab_calendario(datos_sidebar=None)
+        with tab_calendario:
+            renderizar_tab_calendario(datos_sidebar=None)
         with tab_admin:
             renderizar_tab_admin(datos_sidebar=None)
 
