d968a9c18f3596345b3d53b1ca8b5cf158772c48
swimprojecttraining <swimprojecttraining@gmail.com>
2026-07-12 22:08:52 -0400

    Delete utils_security.py

--- utils_security.py
+++
@@ -1,7 +0,0 @@
-# -------------------------------------------------------------
-# FUNCIÓN DE ENCRIPTACIÓN DE CONTRASEÑAS
-# -------------------------------------------------------------
-import hashlib
-
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
