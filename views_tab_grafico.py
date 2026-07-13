caca667d6538004522ec18a090a6f1b9b74bb6b8
swimprojecttraining <swimprojecttraining@gmail.com>
2026-07-13 06:36:40 -0400

    Update views_tab_grafico.py

--- views_tab_grafico.py
+++ views_tab_grafico.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 
