72d4ac540b95a9a1efd539d5f3b116e760d5a8d3
swimprojecttraining <swimprojecttraining@gmail.com>
2026-07-16 10:48:01 -0400

    Update views_tab_router.py

--- views_tab_router.py
+++ views_tab_router.py
@@ -85,7 +85,7 @@ def mostrar_vista_enrutador():
                 padding-top: 0.1rem;
             }
             .main > div {
-                padding-bottom: 200px;
+                padding-bottom: 5rem;
             }
         </style>
     """, unsafe_allow_html=True)
