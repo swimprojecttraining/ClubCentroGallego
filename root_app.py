031965c170f64f298aaef8f818057ffa264f9991
swimprojecttraining <swimprojecttraining@gmail.com>
2026-07-26 08:37:10 -0400

    Update root_app.py

--- root_app.py
+++ root_app.py
@@ -26,7 +26,7 @@ st.markdown(
         
         /* 2. Compactar espacio muerto */
         div[data-testid="stVerticalBlock"] {
-            gap: 0.8rem !important; 
+            gap: 0.3rem !important; 
         }
         .element-container {
             margin-bottom: 4px !important;
